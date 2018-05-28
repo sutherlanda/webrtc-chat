edc5b296885cf8e896b2bb51c20fc3e8f27a8094
Derrick Plotsky <derrickplotsky@gmail.com>
2018-05-27 17:20:24 -0700

    Add Map to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,24 @@
 import React from "react";
+import { Map } from "./geo/Map";
 import SideBar from "./SideBar";
 
 interface AppProps {}
 interface AppState {}
 
 class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+  }
 
-    constructor(props: AppProps) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <SideBar/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <SideBar />
+        Content goes here
+        <Map />
+      </div>
+    );
+  }
 }
 
 export default App;
